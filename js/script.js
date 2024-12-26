72deb91412af807132372ba990a09933357a9852
CeciliaS91 <sanduay91@gmail.com>
2024-12-26 16:04:31 -0300

    maquetado completo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 function mostrarMensaje() {
-    alert('¡Hola! Por favor completa el formulario de Contacto.');
+    alert('¡Hola! Ante cualquier duda, completa nuestro formulario de contacto.');
 }
 
-function confirmacion(){
-    var respuesta = confirm ("¿Deseas enviar esta informacion?");
-    if (respuesta==true){
-        return true;
-    }else {
-        return false;
-    }
+function mensajeClubes() {
+    alert('¡Lo sentimos! Página en mantenimiento.');
 }
+
+
+const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedorCarrito");
+const body = document.querySelector("body");
+
+window.addEventListener("scroll",function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
+
